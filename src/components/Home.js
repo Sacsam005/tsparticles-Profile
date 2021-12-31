4624511d0f69a09de9179e119d4a3fa030b995ba
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-30 20:37:11 -0600

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Section from './Main/Section';
+import Navbar from './Navbar/Navbar';
+
+export default function Home() {
+    return (
+        < >
+            <Navbar />
+            <Header />
+            <Main />
+            <Section />
+            <Footer />
+        </>
+    )
+}
