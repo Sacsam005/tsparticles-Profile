0ff5170680b2b4a563cce70a1600878958ffc8cd
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-12-30 20:35:14 -0600

    Add files via upload

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,10 +10,11 @@ export default function Footer() {
                         <h6>© 2021 | Bina Rai Samal Magar </h6>
 
                         <ul className="footer-item">
-                            <i className="fab fa-facebook fa-2x"></i>
+                            <a href="https://linkedin.com/in/bina-rai-502745162" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
                             <i className="fab fa-twitter fa-2x"></i>
                             <i className="fab fa-instagram fa-2x"></i>
-                            <i className="fab fa-linkedin fa-2x"></i>
+                            <i className="fab fa-facebook fa-2x"></i>
+                            <a href="mailto:binarai532@gmail.com"><i className="far fa-envelope fa-2x"></i></a>
                         </ul>
                     </div>
                 </div>
@@ -25,9 +26,9 @@ export default function Footer() {
                 <div className="footer-content" id="sitemap">
                     <h6>SiteMap | </h6>
                     <ul>
-                        <li className="footer-links"><a href="/">Home</a></li>
-                        <li className="footer-links"><a href="/">About Me</a></li>
-                        <li className="footer-links"><a href="/">Experiences</a></li>
+                        <li className="footer-links"><a href="#home">Home</a></li>
+                        <li className="footer-links"><a href="#about">About Me</a></li>
+                        <li className="footer-links"><a href="#experience">Experiences</a></li>
                     </ul>
                 </div>
             </div>
